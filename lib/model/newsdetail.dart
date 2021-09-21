796e967e3425086154267ed716a5ec973c32a657
abdulhafeez1432 <abdulhafeez1432@gmail.com>
2021-09-21 02:20:07 +0100

    Add files via upload

--- lib/model/newsdetail.dart
+++ lib/model/newsdetail.dart
@@ -1,129 +1,110 @@
-class NewDetails {
+// To parse this JSON data, do
+//
+//     final newsDetail = newsDetailFromMap(jsonString);
+
+import 'dart:convert';
+
+NewsDetail newsDetailFromMap(String str) =>
+    NewsDetail.fromMap(json.decode(str));
+
+String newsDetailToMap(NewsDetail data) => json.encode(data.toMap());
+
+class NewsDetail {
+  NewsDetail({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.imageUrl,
+    required this.url,
+    required this.category,
+    required this.author,
+    required this.site,
+    required this.uploaded,
+    required this.comment,
+  });
+
   int id;
   String title;
   String content;
   String imageUrl;
   String url;
   Category category;
-  Null author;
+  dynamic author;
   Site site;
   String uploaded;
-  List<Comment> comment;
+  List<dynamic> comment;
 
-  NewDetails(
-      {required this.id,
-      required this.title,
-      required this.content,
-      required this.imageUrl,
-      required this.url,
-      required this.category,
-      required this.author,
-      required this.site,
-      required this.uploaded,
-      required this.comment});
+  factory NewsDetail.fromMap(Map<String, dynamic> json) => NewsDetail(
+        id: json["id"],
+        title: json["title"],
+        content: json["content"],
+        imageUrl: json["image_url"],
+        url: json["url"],
+        category: Category.fromMap(json["category"]),
+        author: json["author"],
+        site: Site.fromMap(json["site"]),
+        uploaded: json["uploaded"],
+        comment: List<dynamic>.from(json["comment"].map((x) => x)),
+      );
 
-  factory NewDetails.fromJson(dynamic json) {
-    // print(json);
-
-    List<Comment> posts = [];
-    if (json['results'] != null) {
-      posts = (json['results'] as List).map((x) => Post.fromJson(x)).toList();
-    }
-
-    return PostResponse(
-      posts: posts,
-      nextUrl: json['next'],
-      prevUrl: json['previous'],
-    );
-  }
-
-  String postToJson(List<Post> data) {
-    final dyn = List<dynamic>.from(data.map((x) => x.toJson()));
-    return json.encode(dyn);
-  }
-
-  NewDetails.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    title = json['title'];
-    content = json['content'];
-    imageUrl = json['image_url'];
-    url = json['url'];
-    category:
-    Category.fromJson(json["category"]);
-
-    author = json['author'];
-    site = json['site'] != null ? new Site.fromJson(json['site']) : null;
-    uploaded = json['uploaded'];
-    if (json['comment'] != null) {
-      comment = new List<Null>();
-      json['comment'].forEach((v) {
-        comment.add(new Null.fromJson(v));
-      });
-    }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['title'] = this.title;
-    data['content'] = this.content;
-    data['image_url'] = this.imageUrl;
-    data['url'] = this.url;
-    if (this.category != null) {
-      data['category'] = this.category.toJson();
-    }
-    data['author'] = this.author;
-    if (this.site != null) {
-      data['site'] = this.site.toJson();
-    }
-    data['uploaded'] = this.uploaded;
-    if (this.comment != null) {
-      data['comment'] = this.comment.map((v) => v.toJson()).toList();
-    }
-    return data;
-  }
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "title": title,
+        "content": content,
+        "image_url": imageUrl,
+        "url": url,
+        "category": category.toMap(),
+        "author": author,
+        "site": site.toMap(),
+        "uploaded": uploaded,
+        "comment": List<dynamic>.from(comment.map((x) => x)),
+      };
 }
 
 class Category {
+  Category({
+    required this.id,
+    required this.name,
+    required this.updatedAt,
+    required this.createdAt,
+  });
+
   int id;
   String name;
-  String updatedAt;
-  String createdAt;
+  DateTime updatedAt;
+  DateTime createdAt;
 
-  Category({this.id, this.name, this.updatedAt, this.createdAt});
+  factory Category.fromMap(Map<String, dynamic> json) => Category(
+        id: json["id"],
+        name: json["name"],
+        updatedAt: DateTime.parse(json["updated_at"]),
+        createdAt: DateTime.parse(json["created_at"]),
+      );
 
-  Category.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    updatedAt = json['updated_at'];
-    createdAt = json['created_at'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['updated_at'] = this.updatedAt;
-    data['created_at'] = this.createdAt;
-    return data;
-  }
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+        "updated_at": updatedAt.toIso8601String(),
+        "created_at": createdAt.toIso8601String(),
+      };
 }
 
 class Site {
+  Site({
+    required this.name,
+    required this.logo,
+  });
+
   String name;
   String logo;
 
-  Site({this.name, this.logo});
+  factory Site.fromMap(Map<String, dynamic> json) => Site(
+        name: json["name"],
+        logo: json["logo"],
+      );
 
-  Site.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-    logo = json['logo'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['name'] = this.name;
-    data['logo'] = this.logo;
-    return data;
-  }
+  Map<String, dynamic> toMap() => {
+        "name": name,
+        "logo": logo,
+      };
 }
